a79afbb09ff460de1e658a6e6bfd954058d76f85
Armando Ibarra <70788529+Strako@users.noreply.github.com>
2024-05-23 03:03:03 -0600

    Update LoginForm.jsx

--- Frontend/src/Components/LoginForm/LoginForm.jsx
+++ Frontend/src/Components/LoginForm/LoginForm.jsx
@@ -73,7 +73,7 @@ const LoginForm = () => {
             <p> Registrate</p>
         </div>
     </div>
-    {/*Mapa de google maps */}
+    {/*Mapa de google maps: https://embedgooglemaps.com/es/ */}
     <iframe id="map-canvas" class="map_part" width="600"  height="450"  frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%&amp;height=100%&amp;hl=en&amp;q=slp alameda&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">Powered by <a href="https://embedgooglemaps.com">embed google maps</a> and <a href="http://eucasinos.se/">eu casino</a></iframe>
     </>
   )
